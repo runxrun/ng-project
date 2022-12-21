d538a8ce6fb713f87c3f5685c29742dcdf46974c
NIRUN <nirun@11infotech.com>
2022-12-21 13:45:12 +0700

    [bug] - calendar minus 1 night

--- src/app/pipe/diff-date.pipe.ts
+++ src/app/pipe/diff-date.pipe.ts
@@ -10,8 +10,13 @@ export class DiffDatePipe implements PipeTransform {
   transform(from: NgbDate | null, to: NgbDate | null): number {
     const fromDate = from ? new Date(`${from.year}-${from.month}-${from.day}`) : new Date();
     const toDate = to ? new Date(`${to.year}-${to.month}-${to.day}`) : new Date();
+
+    console.log(fromDate,' - ',toDate);
+
     const nights = moment(toDate).diff(fromDate, 'd');
     return nights > -1 ? nights : 0 ;
   }
+
+
 
 }
