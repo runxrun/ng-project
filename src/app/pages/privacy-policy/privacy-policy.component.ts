ef3ffd8739d165018957aabd9aa8c8a70f2de94a
NIRUN <nirun@11infotech.com>
2023-01-04 15:44:48 +0700

    [update] - validation id card

--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, ValidatorFn, Validators } from '@angular/forms';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./privacy-policy.component.scss']
 })
 export class PrivacyPolicyComponent {
+
+
+  idCardValidator: ValidatorFn = (control: AbstractControl) => {
+
+    const idCard: string | null = control.value;
+    if (idCard) {
+      if (idCard?.length !== 13) {
+        return { idCard: true };
+      }
+
+      let index = 13;
+      let sum = 0;
+
+      for (const num of idCard.substring(0, 12)) {
+        if (isNaN(Number(num))) {
+          return { idCard: true };
+        }
+        sum += Number(num) * index--;
+      }
+      const mod = sum % 11;
+      const checkLastNumber = 11 - mod;
+
+      let strLastNumber = String(checkLastNumber).slice(-1);
+      let strIdCard = String(idCard.charAt(12));
+
+      if (strIdCard !== strLastNumber) {
+        return { idCard: true };
+      }
+
+      //console.log(sum);
+      //console.log(mod);
+      //console.log('11-' + mod + ' = ' + checkLastNumber);
+
+    }
+    return null;
+
+  }
+
+
+  idCard = new FormControl(null, [
+    Validators.required,
+    this.idCardValidator,
+  ]);
+
 
 }
