30d66474d97519b2c836a0e988d76a70805297a5
NIRUN <nirun@11infotech.com>
2023-01-10 17:42:31 +0700

    [update] - booking list

--- src/app/member/pages/my-bookings/my-bookings.component.ts
+++ src/app/member/pages/my-bookings/my-bookings.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class MyBookingsComponent {
 
+
+  items = [
+    {
+      bookingid: 'MA266106712',
+      services: [
+        'HTL BANGKOK',
+        'HTL BKK Massage & Spa',
+        'HTL BKK BKK Restaurant (Buffet)'
+      ],
+      checkin: '2022-11-11',
+      checkout: '2022-11-13',
+      bookedon: '2022-11-10',
+      status: 'Complete',
+    },
+    {
+      bookingid: 'MA266106712',
+      services: [
+        'HTL BANGKOK',
+        'HTL BKK BKK Restaurant (Buffet)'
+      ],
+      checkin: '2023-04-09',
+      checkout: '2023-04-11',
+      bookedon: '2023-01-10',
+      status: 'Complete',
+    },
+  ];
+
+
+
+
 }
